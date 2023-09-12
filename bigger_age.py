d37788afa505ce537dddeb9713ed62e70293f7d5
DevJavanLuiz <144359052+DevJavanLuiz@users.noreply.github.com>
2023-09-11 22:21:25 -0300

    Add files via upload

---
+++ bigger_age.py
@@ -0,0 +1,11 @@
+name = 'Javan'
+last_name = 'Luiz'
+heigth_in_meters = 1.80
+age = 20
+bigger_of_age = age >= 18
+
+print('Name:', name)
+print('Last name:', last_name)
+print("what's your heigth?", heigth_in_meters)
+print('Age:', age)
+print("You's bigger of age?", bigger_of_age)
