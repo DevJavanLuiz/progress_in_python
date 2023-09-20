b5ac59bd1772b69fd91d37aa311a05cf70a09fae
Javan Luiz <144359052+DevJavanLuiz@users.noreply.github.com>
2023-09-20 13:29:17 -0300

    calculo imc

---
+++ imc.py
@@ -0,0 +1,23 @@
+nome = 'Javan'
+
+peso = 94
+
+altura = 1.81
+
+imc = peso / altura **2
+
+
+print('Javan tem 1.81 de altura,está pesando 94kg. Seu IMC è', end=': ')
+print(imc)
+
+nome = 'Sabrina'
+
+peso = 59
+
+altura = 1.65
+
+imc = peso / altura ** 2
+
+
+print('Já o IMC de Sabrina é', end=': ')
+print(imc)
